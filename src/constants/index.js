56994f5eb7820daf8c8c19683135d845dc973968
Graham Kilmon <grahamk@brillianceaudio.com>
2019-06-02 10:48:22 -0400

    fix spacing, add constants file for messages

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const MSG_WINNER = 'Winner: $winner'
+export const MSG_GOTO = 'Go to $place'
+export const MSG_NEXT = 'Next player: $player'
--- src/index.js
+++ src/index.js
@@ -1,59 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Constants from './constants'
 
-  function Square(props){
-      return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-      );
-  }
+
+function Square(props){
+    return (
+    <button className="square" onClick={props.onClick}>
+        {props.value}
+    </button>
+    );
+}
   
-  class Board extends React.Component {
+class Board extends React.Component {
     renderSquare(i) {
-      return (
+        return (
             <Square 
                 value={this.props.squares[i]} 
                 onClick={() => this.props.onClick(i)}
             />
-      );
+        );
     }
-  
+
     render() {
-      return (
+        return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
         </div>
-      );
+        );
     }
-  }
-  
-  class Game extends React.Component {
-      constructor(props){
-          super(props);
-          this.state = {
-              history: [{
-                  squares: Array(9).fill(null),
-              }],
-              stepNumber: 0,
-              xIsNext: true,
-          };
-      }
+}
+
+class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
 
     handleClick(i){
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
@@ -86,8 +88,8 @@ import './index.css';
 
         const moves = history.map((step, move) => {
             const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
+                Constants.MSG_GOTO.replace("$place", "move #" + move) :
+                Constants.MSG_GOTO.replace("$place", "game start");
             
             return (
                 <li key={move}>
@@ -98,54 +100,53 @@ import './index.css';
 
         let status;
         if (winner){
-            status = 'Winner: ' + winner;
+            status = Constants.MSG_WINNER.replace("$winner", winner);
         }else{
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = Constants.MSG_NEXT.replace("$player", (this.state.xIsNext ? 'X' : 'O'));// 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
-      return (
+        return (
         <div className="game">
-          <div className="game-board">
-            <Board 
-                squares={current.squares}
-                onClick={(i) => this.handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
+            <div className="game-board">
+                <Board 
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
         </div>
-      );
+        );
     }
-  }
+}
   
-  // ========================================
-  
-  ReactDOM.render(
+// ========================================
+
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
+);
 
-  function calculateWinner(squares){
-      const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-      ];
+function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 
-      for (let i = 0; i < lines.length; i++){
-          const [a, b, c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-              return squares[a];
-          }
-      }
+    for (let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
 
-      return null;
-  }
-  
+    return null;
+}
